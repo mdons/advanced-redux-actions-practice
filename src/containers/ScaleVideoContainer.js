3d129b434232244fa945c49653781247eca2f932
mdons <michaeljdonnell@gmail.com>
2019-03-28 23:02:48 -0500

    completed

---
+++ src/containers/ScaleVideoContainer.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import { setVideoScale } from "../actions";
+import ScaleVideo from "../components/ScaleVideo";
+
+const mapDispatchToProps = dispatch => {
+  return {
+    set: scale => dispatch(setVideoScale(scale))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ScaleVideo);
---
+++ src/containers/ThermostatContainer.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import Thermostat from "../components/Thermostat";
+
+function mapStateToProps(state) {
+  return {
+    temp: state.currentTemp
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Thermostat);
